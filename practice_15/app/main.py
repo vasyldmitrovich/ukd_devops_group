321874a7e898eebc7a3effcfd9ba6d6ac07381ea
Roketskyi <romanroketskiy05@gmail.com>
2026-04-16 08:10:18 +0000

    Add full Docker Compose stack (Flask + MariaDB + Nginx reverse proxy with persistence and healthchecks)

---
+++ practice_15/app/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+import mysql.connector
+import os
+import time
+
+app = Flask(__name__)
+
+def get_db_connection():
+    # Спроби підключення, поки БД піднімається
+    for i in range(5):
+        try:
+            return mysql.connector.connect(
+                host=os.getenv('DB_HOST', 'db'),
+                user=os.getenv('MYSQL_USER', 'user'),
+                password=os.getenv('MYSQL_PASSWORD', 'pass'),
+                database=os.getenv('MYSQL_DATABASE', 'app_db')
+            )
+        except:
+            time.sleep(2)
+    return None
+
+@app.route('/')
+def hello():
+    return "<h1>Hello bro! Nginx FLASK</h1>"
+
+@app.route('/db')
+def test_db():
+    conn = get_db_connection()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT message FROM init_test")
+        result = cursor.fetchone()
+        conn.close()
+        return jsonify({"status": "Success", "db_value": result[0]})
+    return jsonify({"status": "Error", "message": "Could not connect to DB"}), 500
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

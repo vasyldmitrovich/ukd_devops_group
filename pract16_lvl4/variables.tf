1c24b635ccdfdc0a542920a6c8f2faaa86a4c7d9
dmy3ruk <alinka05alya@gmail.com>
2026-04-23 11:18:59 +0000

    Level 5: Terraform + CI/CD

---
+++ pract16_lvl4/variables.tf
@@ -0,0 +1,11 @@
+variable "MONGODB_ATLAS_PUBLIC_KEY_DMYTRUK" {
+  type = string
+}
+
+variable "MONGODB_ATLAS_PRIVATE_KEY_DMYTRUK" {
+  type = string
+}
+
+variable "MONGODB_ATLAS_ORGANIZATION_ID_DMYTRUK" {
+  type = string
+}

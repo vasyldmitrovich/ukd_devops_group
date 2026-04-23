b8eb1d7690cf49b08b6d282d9e2053bc97500277
OlegChuiko <olegchuiko6@gmail.com>
2026-04-23 07:36:23 +0000

    Level 3: create file via terraform

---
+++ tf-file/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+}
+provider "local" {}
+resource "local_file" "example" {
+  content  = "Hello from Terraform!"
+  filename = "${path.module}/hello.txt"
+}
+
